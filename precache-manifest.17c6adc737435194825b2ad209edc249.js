4df0e44f7f0bf7bb6b0c5308773cee89ed64b5ee
Dang Tuan Huy <dthuya17007@cusc.ctu.edu.vn>
2019-10-09 22:29:23 +0700

    Updates

---
+++ precache-manifest.17c6adc737435194825b2ad209edc249.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd7956ee9ee44359e992cc35fb0e5fcf",
+    "url": "/ReactJS/index.html"
+  },
+  {
+    "revision": "79d832a27b8e281fc9bf",
+    "url": "/ReactJS/static/css/main.d0eb61df.chunk.css"
+  },
+  {
+    "revision": "0cca02fe10f4d25a3546",
+    "url": "/ReactJS/static/js/2.e74b291d.chunk.js"
+  },
+  {
+    "revision": "79d832a27b8e281fc9bf",
+    "url": "/ReactJS/static/js/main.ac376e6f.chunk.js"
+  },
+  {
+    "revision": "0fedfcd6a2f5c3b7098a",
+    "url": "/ReactJS/static/js/runtime-main.4007ebd2.js"
+  }
+]);
--- src/components/search-box/search-box.component.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import './search-box.style.css'
-export const SearchBox = ({placeholder, handleChange}) => (
-    <input type='search' placeholder={placeholder}  className='search'
-    onChange ={handleChange}/>  
-)
